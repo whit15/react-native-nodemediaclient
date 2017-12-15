df5e950a76cacb21a15c4ba814cc5d854970768e
Mingliang Chen <illuspas@gmail.com>
2017-12-16 07:07:19 +0800

    add ios

---
+++ ios/RCTNodeMediaClient/RCTNodePlayerView.h
@@ -0,0 +1,22 @@
+//
+//  RTCNodePlayerView.h
+//  QLive
+//
+//  Created by Mingliang Chen on 2017/11/29.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCTNodePlayerView : UIView
+@property (strong, nonatomic) NSString *inputUrl;
+@property (nonatomic) int bufferTime;
+@property (nonatomic) int maxBufferTime;
+@property (nonatomic) BOOL autoplay;
+@property (strong, nonatomic) NSString *scaleMode;
+
+-(int)start;
+-(int)stop;
+-(int)pause;
+
+@end
---
+++ ios/RCTNodeMediaClient/RCTNodePlayerView.m
@@ -0,0 +1,78 @@
+//
+//  RTCNodePlayerView.m
+//  QLive
+//
+//  Created by Mingliang Chen on 2017/11/29.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#import "RCTNodePlayerView.h"
+#import <NodeMediaClient/NodeMediaClient.h>
+
+@interface RCTNodePlayerView()
+
+@property (strong,nonatomic) NodePlayer *np;
+
+@end
+
+@implementation RCTNodePlayerView
+
+- (id)init {
+  self = [super init];
+  if(self) {
+    _np = [[NodePlayer alloc] init];
+    [_np setPlayerView:self];
+    _autoplay = NO;
+    _inputUrl = nil;
+  }
+  return self;
+}
+
+- (void)setInputUrl:(NSString *)inputUrl {
+  _inputUrl = inputUrl;
+  [_np setInputUrl:inputUrl];
+  if(_autoplay) {
+    [_np start];
+  }
+}
+
+-(void)setBufferTime:(int)bufferTime {
+  _bufferTime = bufferTime;
+  [_np setBufferTime:bufferTime];
+}
+
+-(void)setMaxBufferTime:(int)maxBufferTime {
+  _maxBufferTime = maxBufferTime;
+  [_np setMaxBufferTime:maxBufferTime];
+}
+
+-(void)setScaleMode:(NSString *)scaleMode {
+  int mode = UIViewContentModeScaleToFill;
+  if([scaleMode isEqualToString:@"ScaleAspectFit"]) {
+    mode = UIViewContentModeScaleAspectFit;
+  } else if([scaleMode isEqualToString:@"ScaleAspectFill"]) {
+    mode = UIViewContentModeScaleAspectFill;
+  }
+  [_np setContentMode:mode];
+  
+}
+
+- (void)setAutoplay:(BOOL)autoplay {
+  _autoplay = autoplay;
+  if(_inputUrl) {
+    [_np start];
+  }
+}
+
+-(int)start {
+  return [_np start];
+}
+
+-(int)stop {
+  return [_np stop];
+}
+
+-(int)pause {
+  return [_np pause];
+}
+@end
